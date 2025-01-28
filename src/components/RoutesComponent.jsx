59add682c7c72c1f713bfee982d2fda3c4305b02
AaryanSahu <aryansahu7000@gmail.com>
2025-01-28 17:20:17 +0530

    commit

--- src/components/RoutesComponent.jsx
+++ src/components/RoutesComponent.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
-// import ReminderPage from './ReminderPage';
+import ReminderPage from './ReminderPage';
 
 import New from './New';
 const RoutesComponent = () => {
   const routes = useRoutes([  
     { path: "/new", element: <New /> },
    
-    //  { path: "/reminders", element: <ReminderPage /> },
+     { path: "/reminders", element: <ReminderPage /> },
   ]);
 
   return routes;
